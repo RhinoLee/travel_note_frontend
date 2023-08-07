3e8b51017afd3a72a6b2189c189339ec8712d36c
RhinoLee <picklerhino@gmail.com>
2023-08-07 02:36:19 +0000

    remove unuse code

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import { localStore } from '@/utils/webStorage'
 import { LOGIN_TOKEN } from '@/common/constants'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
-    },
     {
       path: '/entry',
       name: 'entry',
